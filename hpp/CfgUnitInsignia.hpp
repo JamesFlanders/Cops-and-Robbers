69233b568019bba48ef6ef8e1faa30b830c5e583
Jens Caenen <Jens.Caenen@student.pxl.be>
2021-01-22 18:59:47 +0100

    Added insignias to Police

---
+++ hpp/CfgUnitInsignia.hpp
@@ -0,0 +1,10 @@
+class CfgUnitInsignia
+{
+	class PoliceInsignia
+	{
+		displayName = "Police";
+		author = "Flanders";		
+		texture = "images\pol_logo_insignia_greyscale.paa";
+		textureVehicle = "";
+	};
+};
